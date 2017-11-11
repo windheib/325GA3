fe7ff815ab3ca185492982358fc6c53caf57da7f
Sean Cramsey <cramseys@oregonstate.edu>
2017-11-11 15:22:41 -0800

    add skeleton for mst.cpp

---
+++ mst.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+struct node{
+
+};
+
+int int main(int argc, char const *argv[]) {
+  string iPath = "input.txt";
+  ifstream iFile(iPath);
+  /*Read Data*/
+  iFile.close();
+
+  /*Execute Data*/
+
+  oPath = "output.txt";
+  ofstream of(oPath);
+  /*Write Data*/
+  //of << to_string();
+  of.close();
+  return 0;
+}
